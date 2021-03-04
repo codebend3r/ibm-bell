06f3934a800e66c351fa2a95849f44ef18ace052
Chester Rivas <chester.rivas@gmail.com>
2021-03-03 22:21:53 -0500

    implemented API call 🔥
    - created grid
    - added preloader
    - styled the main page
    - added a few new NPM packages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg'
+import { useEffect, useState } from 'react'
+import classNames from 'classnames'
+import BounceLoader from 'react-spinners/BounceLoader'
+
 import './App.css'
 
-function App() {
+const apiUrl = 'https://jsonplaceholder.typicode.com/posts'
+
+const color = '#ff0000'
+
+const App = () => {
+  const [apiResonse, setApiResponse] = useState(null)
+  const [isLoading, setIsLoading] = useState(true)
+  const appClasses = classNames('o-app', {
+    'o-app--loading': isLoading,
+  })
+
+  useEffect(() => {
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        setApiResponse(data)
+        setIsLoading(false)
+      })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={appClasses}>
+      {isLoading ? (
+        <div className="o-loading-container">
+          <BounceLoader color={color} loading size={100} />
+          <pre>Fetching Data</pre>
+        </div>
+      ) : (
+        <>
+          <div className="o-sticky-header">
+            <h1 className="c-title">IBM Bell - React Fetch Test</h1>
+          </div>
+          <div className="o-main-content">
+            <ul className="o-grid-container">
+              {!!apiResonse &&
+                apiResonse.map(({ title, body }, index) => (
+                  <button className="c-button">
+                    <li key={`item-${index}`} className="c-item">
+                      <h3 className="c-item-title">{title}</h3>
+                      <p className="c-item-body">{body}</p>
+                    </li>
+                  </button>
+                ))}
+            </ul>
+          </div>
+        </>
+      )}
     </div>
   )
 }
